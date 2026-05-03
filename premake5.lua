6811a9badfdff93a8de22790d8e51cf80348a174
Guilherme Werner <guilhermewerner@tribufu.com>
2026-05-03 19:05:12 -0300

    Use shared workspace target dir

--- premake5.lua
+++ premake5.lua
@@ -98,19 +98,3 @@ project "Glfw"
         {
             "_GLFW_X11",
         }
-
-    if _ACTION == "vs2026" and os.target() == "windows" then
-        filter { "system:windows", "architecture:x86_64" }
-            targetdir("%{prj.location}/bin/win-x64")
-        filter { "system:windows", "architecture:x86" }
-            targetdir("%{prj.location}/bin/win-x86")
-        filter { "system:windows", "architecture:AARCH64" }
-            targetdir("%{prj.location}/bin/win-arm64")
-        filter { "system:windows", "architecture:ARM" }
-            targetdir("%{prj.location}/bin/win-arm")
-    end
-
-    if not (_ACTION == "vs2026") then
-        filter { "platforms:*" }
-            targetdir("%{prj.location}/bin/%{cfg.platform}")
-    end
